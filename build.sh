57b18c1d538ce597ffe0d86aae8d5c18452efaeb
arnaudbh <arnaudh.pro@gmail.com>
2016-10-15 18:34:52 +0200

    Do not remove image when building, and enjoy using the cache

--- build.sh
+++ build.sh
@@ -5,7 +5,9 @@ IMG_NAME=$(basename $(pwd))
 
 # Make some cleanup
 docker rm -f `docker ps -a | grep "$USER_NAME/$IMG_NAME " | awk '{print $1}'` > /dev/null 2>&1
-docker rmi `docker images  | grep "$USER_NAME/$IMG_NAME " | awk '{print $3}'` > /dev/null 2>&1 
 docker rmi `docker images -f "dangling=true" -q` > /dev/null 2>&1
+
+# Comment out to start from a fresh image (with no cache)
+#docker rmi `docker images  | grep "$USER_NAME/$IMG_NAME " | awk '{print $3}'` > /dev/null 2>&1 
 
 docker build -t "$USER_NAME/$IMG_NAME" .
